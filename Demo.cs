147d1851b01958e52e08cbf13afcfa069d8b621b
aleksandar dejanovsi <dejanovski_a@yahoo.com>
2023-03-11 20:00:57 +0100

    add first test

---
+++ Demo.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using RestSharpDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestSharpDemo
+{
+    public class Demo
+    {
+        public Users GetUsers()
+        {
+            var client = new RestClient("https://reqres.in/");
+            var request = new RestRequest("/api/users?page=2", Method.Get);
+            request.AddHeader("Accept", "application/json");
+            request.RequestFormat = DataFormat.Json;
+
+            RestResponse response = client.Execute(request);
+            var content = response.Content;
+
+            Users users = JsonConvert.DeserializeObject<Users>(content);
+            return users;
+        }
+    }
+}
